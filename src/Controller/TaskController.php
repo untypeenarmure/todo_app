4ab0d8d10508f407c6029a12a53ddaaf5212e6fe
Alexandre Chevalier <alexandre.chevalier@gmail.com>
2021-11-15 14:05:52 +0100

    Récupération des tasks de la bdd dans l'index

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,7 +13,16 @@ class TaskController extends AbstractController
      * @Route("/task/listing", name="task")
      */
     public function index(): Response
-    {
+    {   
+        // On va chercher avec Doctrine le repository de nos Task
+        $repository = $this->getDoctrine()->getRepository(Task::class);
+
+        // Dans le repo, on récupère les entrées
+        $tasks = $repository->findAll();
+        
+        // Affichage dans le var_dumper
+        dd($tasks);
+
         return $this->render('task/index.html.twig', [
             'controller_name' => 'TaskController',
         ]);
