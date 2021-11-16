dae3c7fbef4568c6ac46c289cc18e664224a5c79
Alexandre Chevalier <alexandre.chevalier@gmail.com>
2021-11-16 09:58:54 +0100

    Ajout du tag dans le tableau

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Tag;
 use App\Entity\Task;
 use Faker\Factory;
 use Doctrine\Persistence\ObjectManager;
@@ -14,6 +15,21 @@ class AppFixtures extends Fixture
         // Création d'un nouvel objet Faker
         $faker = Factory::create('fr_FR');
 
+        // Création de 5 Catégories
+        for ($c = 0; $c <5; $c++) {
+            $tag = new Tag;
+
+        // Ajout du nom de la catégorie
+            $tag->setName($faker->colorName());
+
+        // Faire persister les données
+            $manager->persist($tag);
+        }
+
+        $manager->flush();
+
+        $listeTags = $manager->getRepository(Tag::class)->findAll();
+
         // Création entre 15 et 30 tâches aléatoirement
         for ($t= 0; $t < mt_rand(15, 30); $t++) {
 
@@ -24,7 +40,8 @@ class AppFixtures extends Fixture
             $task->setName($faker->sentence(6))
                 ->setDescription($faker->paragraph(3))
                 ->setCreatedAt(new \DateTime())
-                ->setDueAt($faker->dateTimeBetween('now', '6 months'));
+                ->setDueAt($faker->dateTimeBetween('now', '6 months'))
+                ->setTag($faker->randomElement(($listeTags)));
 
             // On fait persister les données
             $manager->persist($task);
