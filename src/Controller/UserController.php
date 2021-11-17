1d6d95fa5c8d927ed30ac9f20acfbc1358a8e859
Amandine Pimentel de Andrade <70423300+G8ite@users.noreply.github.com>
2021-11-17 14:18:09 +0100

    Version 1(Nouveau tableau d'utilisateurs 17112021)

---
+++ src/Controller/UserController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\TaskType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @var UserRepository
+     */
+    private $repository;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $manager;
+
+    public function __construct(UserRepository $repository, EntityManagerInterface $manager) {
+
+        $this->repository = $repository;
+        $this->manager = $manager;
+    }
+
+    /**
+     * @Route("/user/listing", name="user_listing")
+     */
+    public function index(): Response
+    {   
+        // Récupérer les infos de l'utilisateur connecté
+        $user = $this->getUser();
+        //dd($user);
+
+        // Dans le repo, on récupère les entrées
+        $users = $this->repository->findAll();
+        
+        // Affichage dans le var_dumper
+        // dd($users);
+
+        return $this->render('user/index.html.twig', [
+            'users' => $users,
+        ]);
+    }
+
+    /**
+     * @Route("/user/create", name="user_create")
+     * @Route("/user/update/{id}", name="user_update", requirements={"id" = "\d+"})
+     */
+    public function user(User $user = null, Request $request) {
+        
+        if (!$user) {
+            $user = new User;
+            //$user->setCreatedAt(new \DateTime());
+        }
+
+        $form = $this->createForm(TaskType::class, $user, []);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $this->manager->persist($user);
+            $this->manager->flush();
+
+            return $this->redirectToRoute('user_listing');
+        }
+
+        return $this->render('user/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     *
+     * @Route("user/delete/{id}", name="user_delete", requirements={"id" = "\d+"})
+     */
+    public function deleteUser(User $user): Response {
+
+        $this->manager->remove($user);
+        $this->manager->flush();
+
+        return $this->redirectToRoute('user_listing');
+    }
+}
