2a674a63c40b52893ad5638dffe7be394dc6a03a
Alexandre Chevalier <alexandre.chevalier@gmail.com>
2021-11-18 14:47:49 +0100

    Commit O K

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -11,10 +11,12 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
@@ -23,7 +25,7 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', TextType::class, [
+            ->add('email', EmailType::class, [
                 'label'=> 'Email de la grosse tâche'
             ])
             ->add('roles', CollectionType::class, [
@@ -37,8 +39,13 @@ class UserType extends AbstractType
                 'multiple'=> false
                 ]
             ])
-            ->add('password', PasswordType::class, [
-                'label'=> 'Mot de passe'
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'FO SESIR 2 FOI LE MEM',
+                // 'options' => ['attr' => ['class' => 'password-field']],
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
+                
             ])
             ->add('save', SubmitType::class, [
                 'label'=> 'Enregistrer',
